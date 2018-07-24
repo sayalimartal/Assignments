71adcb7a25262f2269d3e334cf25bad8894021ea
sayalimartal <41475386+sayalimartal@users.noreply.github.com>
2018-07-24 07:44:10 +0530

    Update FunctionOverload.java

--- src/FunctionOverload.java
+++ src/FunctionOverload.java
@@ -1,16 +1,17 @@
-class Parent{
-	void display(String name) {
+
+class Parent{                       //Parent class
+	void display(String name) { 
 		System.out.println("Hello "+name+". Welcome to parent class");
 	}
 }
-class Child extends Parent{
-	void display(int i) {
+class Child extends Parent{        //Child class
+	void display(int i) {      
 		System.out.println("Child class "+i);
 	}
 }
 public class FunctionOverload {
 	public static void main(String[] args) {
-		Child child=new Child();
+		Child child=new Child();  // creating an object of child class
 		child.display("Sayali");
 		child.display(2);
 	}
