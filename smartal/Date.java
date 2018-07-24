1af24b20e555af997face8d18e2737072482c49c
sayalimartal <41475386+sayalimartal@users.noreply.github.com>
2018-07-24 08:34:38 +0530

    Update Date.java

--- smartal/Date.java
+++ smartal/Date.java
@@ -1,42 +1,46 @@
+//To display the entered date(dd/mm/yyyy) in the form dd/MonthName/yyyy
 import java.util.*;
 class Date
 {
 	public static void main(String args[])
 	{
-		Scanner sc=new Scanner(System.in);
+		Scanner scanner=new Scanner(System.in);
 		System.out.println("Enter the complete date in the form (DD,MM,YYYY)");
-		String date=sc.nextLine();
-		String[] dmy=date.split(",");
-		System.out.print(dmy[0]+"/");
-		switch(dmy[1])
+		String date=scanner.nextLine();  //Store the date input in a string
+		
+		String[] dmy=date.split(",");    //Separate the string at each comma and store in a string array
+		System.out.print(dmy[0]+"/");    //Display the day
+		
+		switch(dmy[1])                   //Display the month name according to the month number
 		{
 			case "01":System.out.print("January");
-					  break;
+				  break;
 			case "02":System.out.print("February");
-					  break;
+				  break;
 			case "03":System.out.print("March");
-					  break;
+				  break;
 			case "04":System.out.print("April");
-					  break;
+				  break;
 			case "05":System.out.print("May");
-					  break;
+				  break;
 			case "06":System.out.print("June");
-					  break;
+				  break;
 			case "07":System.out.print("July");
-					  break;
-		    case "08":System.out.print("August");
-					  break;
+				  break;
+		        case "08":System.out.print("August");
+				  break;
 			case "09":System.out.print("September");
-					  break;
-		    case "10":System.out.print("October");
-		              break;
+				  break;
+		        case "10":System.out.print("October");
+		                  break;
 			case "11":System.out.print("November");
 			          break;
-		    case "12":System.out.print("December");
-		              break;
+		        case "12":System.out.print("December");
+		                  break;
 			default:System.out.print("Invalid month");
-					break;			  
+			          break;			  
 		}
-		System.out.println("/"+dmy[2]);
+		
+		System.out.println("/"+dmy[2]);   //Display the year
 	}
 }
