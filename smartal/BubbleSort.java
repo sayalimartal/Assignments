f40a9a4eaed87adf75184a0f8d2ed2976caea91e
sayalimartal <41475386+sayalimartal@users.noreply.github.com>
2018-07-24 08:18:03 +0530

    Update BubbleSort.java

--- smartal/BubbleSort.java
+++ smartal/BubbleSort.java
@@ -1,28 +1,29 @@
+//To sort an array using Bubblesort technique
 import java.util.*;
 class BubbleSort
 {
 	public static void main(String args[])
 	{
-		int[] array=new int[15];
+		int[] array=new int[15];    //Create an array of integers of size 15
 		int i,j,swap;
-		Scanner sc=new Scanner(System.in);
+		Scanner scanner=new Scanner(System.in);
 		System.out.println("Enter the array elements");
-		for(i=0;i<15;i++)
-			array[i]=sc.nextInt();
+		for(i=0;i<15;i++)          //Input array elements
+			array[i]=scanner.nextInt();
 		for(i=0;i<14;i++)
 		{
 			for(j=0;j<14;j++)
 			{
-				if(array[j]>array[j+1])
+				if(array[j]>array[j+1])  //Sort the array in descending order
 				{
-					swap=array[j];
+					swap=array[j];   
 					array[j]=array[j+1];
 					array[j+1]=swap;
 				}
 			}				
 		}
 		System.out.println("The sorted array is");
-		for(i=0;i<15;i++)
+		for(i=0;i<15;i++)      //Display the sorted array
 			System.out.print(array[i]+" ");
 	}
 }
