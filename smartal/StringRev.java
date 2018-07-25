16ce5df350c15d3b24ec8a4ca02a003e2f15fcd8
sayalimartal <41475386+sayalimartal@users.noreply.github.com>
2018-07-25 23:57:33 +0530

    Update StringRev.java

--- smartal/StringRev.java
+++ smartal/StringRev.java
@@ -1,20 +1,22 @@
+//Reverse the order of characters in each word
 import java.util.*;
+
 class StringRev
 {
 	public static void main(String args[])
 	{
-		
 		int i;
-		Scanner sc=new Scanner(System.in);
+		Scanner scanner=new Scanner(System.in);
 		System.out.println("Enter a string");
-		String str=sc.nextLine();
-		String[] strcopy=str.split(" ");
+		String str=scanner.nextLine();
+		
+		String[] strcopy=str.split(" "); //Separate the words in the sentence and store in a String array
 		int len=strcopy.length;
+		
 		for(i=0;i<len;i++)
 		{
 			StringBuffer s =new StringBuffer(strcopy[i]);
-			System.out.print(s.reverse()+" ");
+			System.out.print(s.reverse()+" ");  //Reverse each word
 		}
 	}
-}
-		
+}		
