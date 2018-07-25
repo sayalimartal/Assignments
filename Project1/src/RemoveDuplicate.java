f53e6ea659c4cf2d23d600d27ee8afc7f427331b
sayalimartal <41475386+sayalimartal@users.noreply.github.com>
2018-07-25 20:16:29 +0530

    Update RemoveDuplicate.java

--- Project1/src/RemoveDuplicate.java
+++ Project1/src/RemoveDuplicate.java
@@ -1,25 +1,27 @@
+//Remove duplicate characters from the word input
 import java.util.Scanner;
-public class RemoveDuplicate {
-public static void main(String args[])
-{
-	int i,j;
-	char[] str=new char[50];
-	Scanner sc=new Scanner(System.in);
-	System.out.println("Enter a string");
-	str=sc.next().toCharArray();
-	int len=str.length;
-	for(i=0;i<len;i++) {
-		char c=str[i];
-		for(j=i+1;j<len;j++)
-		{
-			if(c==str[j])
-			str[j]=' ';
-		}
-	
-	}
-	String string=new String(str);
-	string=string.replaceAll(" ","");
-	System.out.println("String with duplicate characters removed is "+string);
-}
-}
 
+public class RemoveDuplicate {
+	public static void main(String args[]) {
+		int i,j;
+		char[] str=new char[50];
+		
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Enter a string");
+		str=scanner.next().toCharArray(); //Store the string input in a character array
+		
+		int len=str.length;
+		for(i=0;i<len;i++) {
+			char c=str[i];
+			for(j=i+1;j<len;j++)
+			{
+				if(c==str[j]) 
+				str[j]=' ';
+			}
+		}
+		
+		String string=new String(str);
+		string=string.replaceAll(" ","");
+		System.out.println("String with duplicate characters removed is "+string);
+	}
+}
