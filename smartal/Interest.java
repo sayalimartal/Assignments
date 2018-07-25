51e3966e5a001dc57f669599e17b0e9bcc865635
sayalimartal <41475386+sayalimartal@users.noreply.github.com>
2018-07-25 18:06:52 +0530

    Update Interest.java

--- smartal/Interest.java
+++ smartal/Interest.java
@@ -1,16 +1,20 @@
+//Calculate simple and compound interest for a value
 import java.util.*;
+
 class Interest
 {
 	public static void main(String args[])
 	{
-		Scanner sc=new Scanner(System.in);
+		Scanner scanner=new Scanner(System.in);
 		System.out.println("Enter the principle amount, time period and rate of interest"); 
-		double p=sc.nextDouble();
-		double n=sc.nextDouble();
-		double r=sc.nextDouble();
-		double si=(p*n*r)/100;
-		double ci=(p*Math.pow(1+(r/100),n))-p;
-		System.out.println("Simple interest is "+si);
-		System.out.println("Compound interest is "+ci);
+		double p=scanner.nextDouble();  //Input principle amount,time period and rate of interest
+		double n=scanner.nextDouble();
+		double r=scanner.nextDouble();
+		
+		double si=(p*n*r)/100;   //Calculate simple interest
+		double ci=(p*Math.pow(1+(r/100),n))-p;   //Calculate compound interest
+		
+		System.out.println("Simple interest is "+si);  //Display simple interest
+		System.out.println("Compound interest is "+ci);//Display compound interest
 	}
 }
