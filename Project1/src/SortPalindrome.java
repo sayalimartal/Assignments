1b02c5dd5a7440e2af92cac64a0da12f8dace6fd
sayalimartal <41475386+sayalimartal@users.noreply.github.com>
2018-07-25 22:41:00 +0530

    Update SortPalindrome.java

--- Project1/src/SortPalindrome.java
+++ Project1/src/SortPalindrome.java
@@ -3,46 +3,43 @@ import java.util.*;
 
 public class SortPalindrome {
 	public static void main(String args[]) {
-		int i,j,position=0,swap=0;
-		int[] len=new int[10];
+		int i,j,position=0;
 		int[] location=new int[10];
 		String[] str=new String[10];
 
 		Scanner scanner=new Scanner(System.in);
 		System.out.println("Enter 10 strings");
 		for(i=0;i<10;i++)
-		str[i]=scanner.nextLine();
+			str[i]=scanner.nextLine(); //Input 10 strings
 		
 		for(i=0;i<10;i++)
 		{
 			StringBuffer strcopy =new StringBuffer(str[i]);
 			StringBuffer strrev=strcopy.reverse();
-			if(strrev.toString().equals(str[i])) {
+			if(strrev.toString().equals(str[i])) { //If palindrome,store its position in array 'location'
 				location[position]=i;
 				position++;
 			}
 		}
 		
-		for(i=0;i<position;i++) 
-			len[i]=str[location[i]].length();
+		String[] strPalindrome=new String[position];
 		for(i=0;i<position;i++)
+			strPalindrome[i]=str[location[i]];  //Array of palindromes only 
+		
+		for(i=1;i<position;i++)
 		{	
-			for(j=0;j<position-1;j++)
+			String temp=strPalindrome[i];
+			j=i-1;
+			while(j>=0 && temp.length()>strPalindrome[j].length())  //Sort array in descending order of length of strings
 			{
-				if(len[j]<len[j+1])
-				{
-					swap=len[j];
-					len[j]=len[j+1];
-					len[j+1]=swap;
-					swap=location[j];
-					location[j]=location[j+1];
-					location[j+1]=swap;
-				}
+				strPalindrome[j+1]=strPalindrome[j];
+				j--;	
 			}
+			strPalindrome[j+1]=temp;
 		}
 		
 		System.out.println("Sorted palindromes are");
 		for(i=0;i<position;i++)
-			System.out.println(str[location[i]]);
+			System.out.println(strPalindrome[i]);   //Display sorted palindromes
 	}
 }
