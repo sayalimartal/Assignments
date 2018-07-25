8db87915a29cddb70747d515435e4579f8fa6725
sayalimartal <41475386+sayalimartal@users.noreply.github.com>
2018-07-25 20:28:15 +0530

    Update SortPalindrome.java

--- Project1/src/SortPalindrome.java
+++ Project1/src/SortPalindrome.java
@@ -1,45 +1,46 @@
+//Identify palindromes in input and sort in descending order of characters present in each input
 import java.util.*;
+
 public class SortPalindrome {
-public static void main(String args[])
-{
-	int i,j,position=0,swap=0;
-	int[] len=new int[10];
-	int[] location=new int[10];
-	String[] str=new String[10];
-	
-	Scanner sc=new Scanner(System.in);
-	System.out.println("Enter 10 strings");
-	for(i=0;i<10;i++)
-	str[i]=sc.nextLine();
-	for(i=0;i<10;i++)
-	{
-		StringBuffer strcopy =new StringBuffer(str[i]);
-		StringBuffer strrev=strcopy.reverse();
-		if(strrev.toString().equals(str[i])) {
-			location[position]=i;
-			position++;
-		}
-	}
-	for(i=0;i<position;i++)
-		len[i]=str[location[i]].length();	
-	for(i=0;i<position;i++)
-	{
-		for(j=0;j<position-1;j++)
+	public static void main(String args[]) {
+		int i,j,position=0,swap=0;
+		int[] len=new int[10];
+		int[] location=new int[10];
+		String[] str=new String[10];
+
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Enter 10 strings");
+		for(i=0;i<10;i++)
+		str[i]=scanner.nextLine();
+		
+		for(i=0;i<10;i++)
 		{
-			if(len[j]<len[j+1])
-			{
-				swap=len[j];
-				len[j]=len[j+1];
-				len[j+1]=swap;
-				swap=location[j];
-				location[j]=location[j+1];
-				location[j+1]=swap;
+			StringBuffer strcopy =new StringBuffer(str[i]);
+			StringBuffer strrev=strcopy.reverse();
+			if(strrev.toString().equals(str[i])) {
+				location[position]=i;
+				position++;
 			}
 		}
+		
+		for(i=0;i<position;i++) {
+			len[i]=str[location[i]].length();	
+			for(j=0;j<position-1;j++)
+			{
+				if(len[j]<len[j+1])
+				{
+					swap=len[j];
+					len[j]=len[j+1];
+					len[j+1]=swap;
+					swap=location[j];
+					location[j]=location[j+1];
+					location[j+1]=swap;
+				}
+			}
+		}
+		
+		System.out.println("Sorted palindromes are");
+		for(i=0;i<position;i++)
+			System.out.println(str[location[i]]);
 	}
-	System.out.println("Sorted palindromes are");
-	for(i=0;i<position;i++)
-		System.out.println(str[location[i]]);
 }
-}
-
