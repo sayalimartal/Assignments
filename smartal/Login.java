bbaa023a41c9a0eb1cccc9a4eea516cf21b9386e
sayalimartal <41475386+sayalimartal@users.noreply.github.com>
2018-07-25 18:34:57 +0530

    Update Login.java

--- smartal/Login.java
+++ smartal/Login.java
@@ -1,31 +1,33 @@
+//Allow valid user login
 import java.util.*;
 class Login
 {
 	public static void main(String args[])
 	{
 		int flag=0,count=0;
-		Scanner sc=new Scanner(System.in);
-		while(flag==0)
+		Scanner scanner=new Scanner(System.in);
+		
+		while(flag==0)  //For invalid login attempts
 		{
 			System.out.println("Enter the login name and password");
-			String loginName=sc.nextLine();
-			String password=sc.nextLine();
-			if(loginName.equals("sayali") && password.equals("say@123"))
+			String loginName=scanner.nextLine();
+			String password=scanner.nextLine();
+			
+			//If valid login credentials entered, print Welcome message along with username
+			if(loginName.equals("sayali") && password.equals("say@123")) 
 			{
 				System.out.println("Welcome "+loginName);
 				flag=1;
 				break;
 			}
-			else
+			else  //Keep track of invalid login attempts
 				count++;
-			if(count==3)
+			
+			if(count==3)   //Terminate program after three invalid attempts
 			{
 				System.out.println("Contact Admin");
 				break;
 			}
 		}
 	}
-}
-		
-		
-		
+}		
